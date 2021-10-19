ad625868de431db29bfe5a28d270f3954f21e123
nishantgrg21 <nishantgrg21@gmail.com>
2021-10-19 14:42:32 +0530

    ui changes

--- src/app/pages/dashboard/portfolio-overview/portfolio-overview.component.ts
+++ src/app/pages/dashboard/portfolio-overview/portfolio-overview.component.ts
@@ -8,24 +8,39 @@ import portfolioNewData from '../portfolio-overview/portfolio-newdata.json';
   styleUrls: ['./portfolio-overview.component.css']
 })
 export class PortfolioOverviewComponent implements OnInit {
+  [x: string]: any;
 
   public PortfolioData1:{image:string,title:string,amount:string, percent:string}[] = portfolioData;
   public PortfolioData2:{image:string,title:string,amount:string, percent:string}[] = portfolioNewData;
 
-   dropdownList=[
-     "Bucket 1", "Bucket 2" ,"Bucket3"
+   dropdownList: any =[
+     "Bucket 1", "Bucket 2" ,"Bucket 3","Bucket 4"
    ]
+   dropdownList2=[
+    "January'21", "Febuary' 21" ,"March' 21 ","April' 21"
+  ]
   constructor() { }
+ 
 
   ngOnInit(): void {
+
+  }
+ 
+  selectedValue: any;
+ 
+  onSelectList(event: any){
+  // this.selectedValue = event.trigger.item;
+  this.selectedValue =this.event.item;
+  console.log( this.selectedValue);
+
   }
 
-  // drpDownBtnValues:string='';
-  // dropdwnChange (event: any){
-  //    this.drpDownBtnValues= event.target.valuesss;
-
-  // }
+  
 
   
 
 }
+function item(item: any): any {
+  throw new Error('Function not implemented.');
+}
+
