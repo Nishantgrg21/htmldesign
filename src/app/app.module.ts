fe2b9b7e26de26613870f3039abafb1170c95b68
nishantgrg21 <nishantgrg21@gmail.com>
2021-10-26 22:58:35 +0530

    table ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import {HttpClientModule } from '@angular/common/http'
     AppRoutingModule,
     SharedModule,
     LayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,16 +19,29 @@ export class HomeComponent implements OnInit {
 
   users! : Users[];
 
-
+  SingleContent:any;
   ngOnInit(): void {
     this.ApiServiceData.getRequest().subscribe(
       (res:any) => {
         this.users = res.data;
         console.log(res);
       }) 
+  this.SingleContent=true
   }
 
+  // SelectedValue :any='userprofile';
+  // onChangeValue(userprofile:any){
+  //   this.SelectedValue = userprofile;
+  //   console.log('SelectedValue');
+  // }
 
+  display = "none";
+  openModal() {
+    this.display = "block";
+  }
+  closeModal() {
+    this.display = "none";
+  }
 
 }
 
